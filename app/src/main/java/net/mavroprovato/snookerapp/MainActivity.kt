abc09f0eb36ee02dc79954dfa4ca38f1547a6fb2
Kostas Kokkoros <mavroprovato@gmail.com>
2018-12-09 04:40:42 +0200

    Fetch event data

--- app/src/main/java/net/mavroprovato/snookerapp/MainActivity.kt
+++ app/src/main/java/net/mavroprovato/snookerapp/MainActivity.kt
@@ -1,12 +1,49 @@
 package net.mavroprovato.snookerapp
 
+import android.os.AsyncTask
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import net.mavroprovato.snookerapp.api.eventsInSeason
 
+/**
+ * The main application activity.
+ */
 class MainActivity : AppCompatActivity() {
 
+    /**
+     * Create the activity.
+     *
+     * @param savedInstanceState The saved bundle instance.
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        FetchEventsTask().execute(2018)
+    }
+
+    /**
+     * A asynchronous task that fetches all events in a year.
+     */
+    inner class FetchEventsTask : AsyncTask<Int, Void, List<String>>() {
+        /**
+         * Fetch the event data.
+         *
+         * @param params The years requested. Only the first year provided is taken into accout.
+         * @return An array with all the events.
+         */
+        override fun doInBackground(vararg params: Int?): List<String> {
+            return eventsInSeason(params[0])
+        }
+
+        /**
+         * Called when the data are fetched and sets the text view data.
+         *
+         * @param result The result array.
+         */
+        override fun onPostExecute(result: List<String>?) {
+            result?.forEach { tv_events.append(it + "\n\n\n") }
+        }
     }
 }
